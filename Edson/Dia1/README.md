a67cff29296329d295d3278dcf26330414d58b0b
edspm <edson_lopez@hotmail.com.ar>
2016-08-02 23:31:45 -0400

    task2 02/08/2016

---
+++ Edson/Dia1/README.md
@@ -0,0 +1,12 @@
+# ruby_cucumber_course
+
+Practice for ruby and cucumber for AT02
+
+Folder for scripts and code for:
+
+Edson Percy Lopez Cortez
+
+
+
+#Send MSG	
+I sent this msg becouse i can't open the archive with the question
---
+++ Edson/Dia2/task2.rb
@@ -0,0 +1,25 @@
+
+a=10
+b=2
+c=3
+d=5
+e= a/d
+
+puts "si a=10 y b=2 la resta es:,  si d = 5 la operacion de a / d es:"
+puts "resta: #{a-b}"
+puts "operacion: #{e}"
+puts " "
+puts "------------------------------------------------------------"
+puts " "
+puts "convert seconds in minutes, seconds in hours and second in days"
+puts " "
+
+seg = 80000
+min = seg/60
+hrs = seg/3600
+dia = seg/86400
+puts "#{seg} segundos, en minutos es #{min} minutos"
+puts "#{seg} segundos, en horas es #{hrs} horas"
+puts "#{seg} segundos, en dia es #{dia} dias"
+
+
