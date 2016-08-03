b9a9a788b672ca06b71ff8c93744092709724867
Edson Lopez Cortez <EdsonLopez@Fundacion-jala.local>
2016-08-03 17:45:39 -0400

    task2.2 08/03/2016

--- Edson/Dia2/task2.rb
+++ Edson/Dia2/task2.rb
@@ -4,6 +4,7 @@ b=2
 c=3
 d=5
 e= a/d
+<<<<<<< HEAD
 f= 2
 
 puts "resta #{a} y #{b}: #{a-b}"
@@ -42,3 +43,24 @@ puts " "
 puts "Signo and : #{a and b}"
 puts "Signo or : #{a or d}"
 puts "Signo ! : #{!(c == d)}"
+=======
+
+puts "si a=10 y b=2 la resta es:,  si d = 5 la operacion de a / d es:"
+puts "resta: #{a-b}"
+puts "operacion: #{e}"
+puts " "
+puts "------------------------------------------------------------"
+puts " "
+puts "convert seconds in minutes, seconds in hours and second in days"
+puts " "
+
+seg = 80000
+min = seg/60
+hrs = seg/3600
+dia = seg/86400
+puts "#{seg} segundos, en minutos es #{min} minutos"
+puts "#{seg} segundos, en horas es #{hrs} horas"
+puts "#{seg} segundos, en dia es #{dia} dias"
+
+
+>>>>>>> a67cff29296329d295d3278dcf26330414d58b0b
